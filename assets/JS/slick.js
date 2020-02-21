d97ade7c2bf0edac9630e4ef66f0221b23503e86
Denys Marytchak <denys.marytchak@gmail.com>
2020-02-22 00:23:04 +0100

    feat: add carouselle
    - Add slick carouselle in order to make buttons work

---
+++ assets/JS/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#slick-carousele').slick({
+        prevArrow: $('#prev'), /* custom arrow */
+        nextArrow: $('#next'), /* custom arrow */
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: true,
+        autoplay: false,
+      });
+    });
