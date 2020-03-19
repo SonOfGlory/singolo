f78d058e88a2e873bd45a9399af3ff222007133a
Denys Marytchak <denys.marytchak@gmail.com>
2020-03-19 22:00:39 +0100

    init: singolo DOM

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
+/* ======= Header menu part ===== */
+
+document.addEventListener('scroll', onscroll);
+
+function onscroll (e) {
+  const scrollPosition = window.scrollY;
+  const allParts = document.querySelectorAll('body > header, body > main > section, body > footer');
+  console.log(scrollPosition);
+
+  allParts.forEach((element) => {
+    console.log(element.getAttribute('id'));
+  });
+}
+
 const headerMenu = document.querySelector("body > header > ul");
-const portfolioMenu = document.querySelector("body > main > section.portfolio > ul.portfolio_menu");
 
 headerMenu.onclick = event => { headerMenu.querySelectorAll('li > a').forEach(element => element.classList.remove('menu-active'));
     event.target.classList.add('menu-active');
 };
 
-// menu.addEventListener('click', (event) => { menu.querySelectorAll('li > a').forEach(element => element.removeAttribute('id', 'menu-active'));
-//     event.target.setAttribute('id','menu-active');
-// });
+/* ====== Slider menu part ==== */
 
-// nav.onclick = e => e.preventDefault() || e.target.tagName!='A'? 0 :
-//             document.querySelector(e.target.getAttribute('href'))
-//               .scrollIntoView({behavior: 'smooth'});
+const slider_buttons = document.querySelectorAll('.slider_button')
+const slide_container = document.querySelector('.carousele')
+const slider_elements = document.querySelectorAll('.carousele>div')
 
-// всем картинках присваеваем классы в соответствии с названиями пунктов меню - руками в HTML
+slider_elements[0].classList.add('current')
+slider_elements[0].dataset.show = 'current'
+slider_elements[1].dataset.show = 'right'
 
-//кликаем на опцию меню: 
+slider_buttons.forEach(button => button.onclick = slideBtnHandler)
+
+function slideBtnHandler(e) {
+  if (slide_container.classList.contains('busy')) return
+
+  const current_slide = document.querySelector('.slide[data-show=current]')
+  const new_slide = document.querySelector('.slide:not([data-show=current])')
+  const direction = this.dataset.arrow=='right'? 'left' : 'right'
+
+  new_slide.dataset.show = this.dataset.arrow
+
+  setTimeout(function () {
+    slide_container.classList.add('busy')
+    current_slide.dataset.show = direction
+    new_slide.dataset.show = 'current'
+    new_slide.ontransitionend = function () {
+      slide_container.classList.remove('busy')
+    }
+  }, 50)
+}
+ 
+
+/* ====== Portfolio menu part ==== */
+
+const portfolioMenu = document.querySelector("body > main > section.portfolio > ul.portfolio_menu");
+const imagesMenu = document.querySelector('ul.portfolio_images');
+
 portfolioMenu.onclick = event => {
 
     portfolioMenu.querySelectorAll('li > button').forEach(element => element.classList.remove('portfolio-active'));
     event.target.classList.add('portfolio-active');
 
-    document.querySelector('ul.portfolio_images').dataset.filter = event.target.id;
+    imagesMenu.dataset.filter = event.target.id;
 
     /* 
     //узнаем пункт меню на который произошел клик: event.target и соответственно event.target.id
@@ -38,6 +77,11 @@ portfolioMenu.onclick = event => {
         foundImages.forEach(element => element.style.display = 'list-item');
     } 
     */
-    
+}
+
+imagesMenu.onclick = (e) => {
+  if (e.target.classList.contains('portfolio_images')) return;
+  imagesMenu.querySelectorAll('li').forEach(element => element.classList.remove('portfolio_images_clicked'));
+  e.target.parentNode.classList.add('portfolio_images_clicked');
 
 }
