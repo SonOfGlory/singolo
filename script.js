c55b1ee414d2f4e704f030a9c8cd44f65d596020
Denys Marytchak <denys.marytchak@gmail.com>
2020-03-27 00:27:49 +0100

    feat: low-resulution menu toggle on burger
    -implement bugger-menu toggle for screen width lower than 620px
    -still no dark background implemented

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ onscroll = () => {
   });
 }
 
+/* == Toggler low-resolution part == */
+
+const menu_toggler = document.querySelector("#header > div.menu-toggler");
+const menu_togglers_set = document.querySelectorAll('[data-menuswitch]');
+
+menu_toggler.onclick = () => {
+  if (document.querySelectorAll('[data-menuswitch="hide"]').length === 0) {
+    menu_togglers_set.forEach(toggler => {
+      toggler.dataset.menuswitch = 'hide';
+    }
+  )}
+  else menu_togglers_set.forEach(toggler => {
+      toggler.dataset.menuswitch = '';
+    }
+  )
+}
+
 /* ====== Slider menu part ==== */
 
 const slider_buttons = document.querySelectorAll('.slider_button')
