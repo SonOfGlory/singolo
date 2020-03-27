d532f8af712681e5f6ab27f4dce22f86444d39ed
Denys Marytchak <denys.marytchak@gmail.com>
2020-03-27 01:05:27 +0100

    fix: drop-right, singolo text center
    - drop-right menu shadow removed
    - singolo* text better centered

--- script.js
+++ script.js
@@ -25,9 +25,9 @@ const menu_toggler = document.querySelector("#header > div.menu-toggler");
 const menu_togglers_set = document.querySelectorAll('[data-menuswitch]');
 
 menu_toggler.onclick = () => {
-  if (document.querySelectorAll('[data-menuswitch="hide"]').length === 0) {
+  if (document.querySelectorAll('[data-menuswitch="hidden"]').length === 0) {
     menu_togglers_set.forEach(toggler => {
-      toggler.dataset.menuswitch = 'hide';
+      toggler.dataset.menuswitch = 'hidden';
     }
   )}
   else menu_togglers_set.forEach(toggler => {
